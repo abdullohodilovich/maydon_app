e87dd11010ffb53323a331bfe26a2d9a7468be01
Abdulloh <abdullohabduqodirov0006@gmail.com>
2023-11-02 01:48:16 +0500

    auth full commit two

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:maydon_app/src/presentation/pages/sign_in_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maydon_app/blocs/auth_bloc/auth_bloc.dart';
+import 'package:maydon_app/pages/get_started_page.dart';
+import 'package:maydon_app/pages/home_page.dart';
+import 'package:maydon_app/services/auth_services.dart';
 
 class MaydonApp extends StatelessWidget {
   const MaydonApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(useMaterial3: true),
-      darkTheme: ThemeData.dark(useMaterial3: true),
-      themeMode: ThemeMode.light,
-      home: const SignInPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_) => AuthBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(useMaterial3: true),
+        darkTheme: ThemeData.dark(useMaterial3: true),
+        themeMode: ThemeMode.light,
+        home: StreamBuilder<User?>(
+          initialData: null,
+          stream: AuthService.auth.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.data != null) {
+              debugPrint("Data :: ${snapshot.data}");
+              return const HomePage();
+            } else {
+              return const GetStartedPage();
+            }
+          },
+        ),
+      ),
     );
   }
 }
